a79f91f3b93da441ff17a6b35785b3b16046f6e7
Imdadul Haque Ohi <imdadul0202@gmail.com>
2023-12-07 19:41:04 +0600

    update

--- inputOutput.dart
+++ inputOutput.dart
@@ -1,7 +1,18 @@
 import 'dart:io';
+
 void main() {
+  // integer input and print
+  stdout.write("Enter a integer number: "); // stdout.write for without endline
   int? number = int.parse(stdin.readLineSync()!);
   print(number);
+
+  // double input and print
+  stdout.write("Enter Double Number: "); // stdout.write for without endline
   double? fraction = double.parse(stdin.readLineSync()!);
   print(fraction);
+  
+  // String input and print
+  stdout.write("Enter Your Name: "); // stdout.write for without endline
+  String? name = stdin.readLineSync();
+  print("My Name is $name!\nWelcome to my accound!!");
 }
