be3966bc56317b344518b83d652a37e1a1053efc
Imdadul Haque Ohi <imdadul0202@gmail.com>
2023-12-07 19:27:59 +0600

    basic

---
+++ inputOutput.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+void main() {
+  int? number = int.parse(stdin.readLineSync()!);
+  print(number);
+  double? fraction = double.parse(stdin.readLineSync()!);
+  print(fraction);
+}
