029042e7d833a57bb6c112f84a1dcfb862b93264
Imdadul Haque Ohi <imdadul0202@gmail.com>
2023-12-07 21:51:16 +0600

    update

--- function.dart
+++ function.dart
@@ -7,16 +7,33 @@ void main() {
   display2();
   second = int.parse(stdin.readLineSync()!);
   print('The sum is = ${Sum(first, second)}');
+  sumOftwo(first, second);
+  printNumber1(1, 2);
+  printNumber2(1, 2, n3: 4, n4: 5);
 }
+
 // int type funciton with return
 int Sum(int first, int second) {
   return (first + second);
 }
-// void type function without return 
+
+// void type function without return
 void display1() {
   stdout.write("Enter 1st number: ");
 }
 
 void display2() {
   stdout.write("Enter 2nd number: ");
+}
+
+// arrow functon
+void sumOftwo(int a, int b) => print("Sum = ${a + b}");
+// optional positional parameter function
+void printNumber1(int n1, int n2, [n3, n4]) {
+  print("n1 = $n1, n2 = $n2, n3 = $n3, n4 = $n4");
+}
+
+// optional named parameter function
+void printNumber2(int n1, int n2, {required n3, n4}) {
+  print("n1 = $n1, n2 = $n2, n3 = $n3, n4 = $n4");
 }
--- testFile.dart
+++ testFile.dart
@@ -1,9 +1,14 @@
+import 'dart:ffi';
 import 'dart:io';
 
 void main() {
-  int n1 = 10;
+  /* int n1 = 10;
   int n2 = 20;
   print("n1 + n2 = ${n1 + n2}");
   print(n1++);
-  print(++n1);
+  print(++n1); */
+  String s = "ohi";
+  for (int i = 0; i < 3; i++) {
+    stdout.write(s[i]);
+  }
 }
