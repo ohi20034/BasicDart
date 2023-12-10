9483e818abdfcf732f8d1e4ca68509b7672987d2
Imdadul Haque Ohi <imdadul0202@gmail.com>
2023-12-10 21:08:33 +0600

    update

--- testFile.dart
+++ testFile.dart
@@ -7,8 +7,10 @@ void main() {
   print("n1 + n2 = ${n1 + n2}");
   print(n1++);
   print(++n1); */
-  String s = "ohi";
+  /* String s = "ohi";
   for (int i = 0; i < 3; i++) {
     stdout.write(s[i]);
-  }
+  } */
+  List<int> list = [1, 2, 3];
+  print(list);
 }
